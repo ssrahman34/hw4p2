89aa31bd75a92427866d699e0c49c88a27ed50c5
ssrahman34 <ssrahman@ucdavis.edu>
2018-05-16 07:03:27 +0000

    Inserted in the DB correctly

--- server/loadNames.js
+++ server/loadNames.js
@@ -1,6 +1,6 @@
 var url = require('url');
 var http = require('http');
-//var sizeOf = require('image-size');
+var sizeOf = require('image-size');
 const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('./PhotoQ.db');
 imgList=[];
@@ -21,25 +21,46 @@ function loadImageList () {
 		for (var i = 1; i < imgList.length; i++){
 		console.log(imgList[i]);
 		//var cmdStr="INSERT INTO photoTags VALUES(i, 'Dtring', 0,0,'','')"
-		fillDB(imgList[i]);
+		fillDB(imgList[i], i);
 		}
-		function fillDB(url){
-		urlList = url.split('/');
-		console.log(urlList[urlList.length -1]);				
+		function fillDB(imgUrl, i){
+		var width;
+		var height;
+		var options = url.parse(imgUrl);
+		http.get(options, function (response) {
+  		var chunks = [];
+  		response.on('data', function (chunk) {
+   		 chunks.push(chunk);
+  		}).on('end', function() {
+    			var buffer = Buffer.concat(chunks);
+			dim=sizeOf(buffer);
+			width = dim.width;
+			height = dim.height; 
+			//console.log(width, height); 
+		console.log("i"+ i);
+		urlList = imgUrl.split('/');
+		console.log("Widht and h"+width, height);
+		var fileName = urlList[urlList.length -1];
+		fileName = decodeURIComponent(fileName);
+console.log(fileName);				
+		    var cmdStr = "INSERT OR REPLACE INTO photoTags VALUES(" + i + ", '" + fileName + "','"+width+"','"+height+"', '', '')";
+		    //var cmdStr = "INSERT INTO photoTags VALUES(" + i + "," + fileName + "," + width + "," + height+ ",'','')"
+		db.run(cmdStr, dbCallback);
+		});
+		});
 		}
-//		var cmdStr="INSERT INTO photoTags VALUES(10, 'Dtring', 0,0,'','')"
-//		db.run(cmdStr, dbCallback); 
-//		function dbCallback(err) {
-//		   if (err) { console.log(err); }
-//		   else{
-//			console.log("Inserted"); 
-//		}  
-//	}
+		dumpDB();
+		function dbCallback(err) {
+		   if (err) { console.log(err); }
+		   else{
+			console.log("Inserted row"); 
+		   }  
+		}
 }//loadIMG
 }//read
-/*function dumpDB() {
+function dumpDB() {
 	console.log("in dump");
-	db.all ( 'SELECT * FROM photoTags', dataCallback);
+	db.all( 'SELECT * FROM photoTags', dataCallback);
 		function dataCallback( err, data ) {
 		  console.log(data) 
-		} } */
+		} } 
